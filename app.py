9382ababfca4ca702681d8ef4bea1cc1c70aee8e
AndreasNygaardSchioett <112076744+AndreasNygaardSchioett@users.noreply.github.com>
2023-04-17 13:29:58 +0200

    push

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 import requests
 import json
 import os
+import sqlite3
 
 
 app = Flask(__name__)
+
+dir = os.path.dirname(__file__)
+db = os.path.join(dir, 'Clever.db')
 
 @app.route("/")
 def startside():
@@ -42,6 +46,55 @@ def co2udledning():
 def page_not_found(e):
     return render_template("menu.html")
 
+#function that gets the data from clever.db opklader and sends it to the html file
+@app.route("/data")
+def data():
+    with sqlite3.connect(db) as conn:
+        c = conn.cursor()  
+        c.execute("SELECT * FROM oplader")
+        oplader = c.fetchall()
+        print(oplader)
+    return render_template("data.html", oplader=oplader)
+
+
+
+# DML inset profile data into database in table kunde
+@app.route('/create', methods=['POST'])
+def create():
+    navn = request.form.get('Navn')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    adresse = request.form.get('adresse')
+    data = (navn, email, password, adresse)
+    
+    with sqlite3.connect(db) as conn:
+        c = conn.cursor()
+        c.execute("INSERT INTO kunde (navn, email, password, adresse) VALUES (?, ?, ?, ?)", data)
+    return redirect('/')
+
+
+
+
+# DML update oplader udelukende in database in table oplader
+@app.route('/update', methods=['POST'])
+def update():
+    solcelle = request.form.get('solcelle')
+    if solcelle == "true":
+        new_value = 1
+        old_value = 0
+    else:
+        new_value = 0
+        old_value = 1
+
+    print(new_value)
+    print(solcelle)
+    data = (new_value, old_value)
+    print(data)
+
+    with sqlite3.connect(db) as conn:
+        c = conn.cursor()
+        c.execute("UPDATE oplader SET solcelle=? WHERE solcelle=?", data)
+    return redirect('/')
 
 
 
--- ddl.py
+++ ddl.py
@@ -4,8 +4,20 @@ import sqlite3
 create_kunde_table = """
 CREATE TABLE IF NOT EXISTS kunde (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    navn TEXT,
+    email TEXT,
+    password TEXT,
+    adresse TEXT
+);
 """
 
+create_oplader_table = """
+CREATE TABLE IF NOT EXISTS oplader (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    strømkapacitet INTEGER,
+    solcelle INTEGER
+);
+"""
 create_bil_table = """
 CREATE TABLE IF NOT EXISTS område (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -13,45 +25,11 @@ CREATE TABLE IF NOT EXISTS område (
 ); 
 """
 
-create_chop_table = """
-CREATE TABLE IF NOT EXISTS chip (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
 
-); 
-""" 
 
-create_bud_table = """
-CREATE TABLE IF NOT EXISTS bud (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    restaurant_id INTEGER, 
-    ordre_id INTEGER,
-    område_id INTEGER,
-    FOREIGN KEY (restaurant_id) REFERENCES restaurant (id),
-    FOREIGN KEY (ordre_id) REFERENCES ordre (id),
-    FOREIGN KEY (område_id) REFERENCES område (id) 
- 
-); 
-"""
-create_restaurant_table = """
-CREATE TABLE IF NOT EXISTS restaurant (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    adresse TEXT,
-    restaurantnavn TEXT,
-    kunde_id INTEGER,  
-    ordre_id INTEGER,
-    bud_id INTEGER,
-    område_id INTEGER, 
-    FOREIGN KEY (kunde_id) REFERENCES kunde (id),
-    FOREIGN KEY (område_id) REFERENCES område (id),
-    FOREIGN KEY (ordre_id) REFERENCES ordre (id),
-    FOREIGN KEY (bud_id) REFERENCES bud (id) 
-); 
-"""
-
-with sqlite3.connect('OrderYOYO.db') as conn:
+with sqlite3.connect('Clever.db') as conn:
     c = conn.cursor()
-    c.execute(create_bud_table)
     c.execute(create_kunde_table)
-    c.execute(create_ordre_table)
-    c.execute(create_område_table)
-    c.execute(create_restaurant_table)
+    c.execute(create_oplader_table)
+    c.execute("INSERT INTO oplader (strømkapacitet, solcelle) VALUES (100, 0)")
+
